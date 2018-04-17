1e95c6ec93ee67b5456cd4e933a176b4f124f446
Kenzaburo Miyawaki <kenzaburo.miyawaki@oit.ac.jp>
2018-04-17 11:56:00 +0900

    typedef 少し変更

--- src/rsj_pointcloud_test_node.cpp
+++ src/rsj_pointcloud_test_node.cpp
@@ -1,7 +1,8 @@
 #include <ros/ros.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+typedef pcl::PointXYZ PointT
+typedef pcl::PointCloud<PointT> PointCloud;
 
 class rsj_pointcloud_test_node
 {
