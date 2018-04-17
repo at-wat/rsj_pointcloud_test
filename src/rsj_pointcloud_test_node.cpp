441d2c8e2023d92ac480aa51b6b77a46f8709bfd
Kenzaburo Miyawaki <kenzaburo.miyawaki@oit.ac.jp>
2018-04-17 10:54:26 +0900

    最初のコミット

---
+++ src/rsj_pointcloud_test_node.cpp
@@ -0,0 +1,41 @@
+#include <ros/ros.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+class rsj_pointcloud_test_node
+{
+private:
+  ros::Subscriber sub_points;
+
+  void cb_points(const PointCloud::ConstPtr &msg)
+  {
+  }
+
+public:
+  rsj_pointcloud_test_node()
+  {
+    ros::NodeHandle nh("~");
+    sub_points = nh.subscribe("/camera/depth_registered/points", 5,
+                              &rsj_pointcloud_test_node::cb_points, this);
+  }
+
+  void mainloop()
+  {
+    ROS_INFO("Hello Point Cloud!");
+
+    ros::Rate rate(30.0);
+    while (ros::ok())
+    {
+      ros::spinOnce();
+      rate.sleep();
+    }
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "rsj_pointcloud_test_node");
+  rsj_pointcloud_test_node pointcloud_test;
+  pointcloud_test.mainloop();
+}
